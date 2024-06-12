927ffff6d1e4aa30789e43e08c195f87aff06bff
Yevhenii Turianskyi <zhenya5990@gmail.com>
2024-06-12 22:20:02 +0300

    Create TransactionRepositoryInterface method stubs

--- app/Repositories/Interfaces/TransactionRepositoryInterface.php
+++ app/Repositories/Interfaces/TransactionRepositoryInterface.php
@@ -2,7 +2,31 @@
 
 namespace App\Repositories\Interfaces;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface TransactionRepositoryInterface
 {
+    /**
+     * @param array $filters
+     * @return Collection
+     */
+    public function getAllTransactions(array $filters): Collection;
 
+    /**
+     * @param int $transactionId
+     * @return mixed
+     */
+    public function getTransactionById(int $transactionId): mixed;
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function createNewTransaction(array $data): mixed;
+
+    /**
+     * @param int $transactionId
+     * @return void
+     */
+    public function deleteTransactionById(int $transactionId): void;
 }
