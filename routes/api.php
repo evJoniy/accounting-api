0f6aa04251c988db00a2e286775670b279b36c84
Yevhenii Turianskyi <zhenya5990@gmail.com>
2024-06-12 20:52:37 +0300

    Create TransactionController and API resource for it

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('transactions', TransactionController::class);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
